8ba7f3b94018e5da770a5cef6930b0fc6b8b1941
lucasAmbiensys <lucas.amorim@ambiensys.com.br>
2025-10-24 10:24:49 -0300

    feat: Configure Fastify plugins and Swagger
    This commit configures several Fastify plugins:
    
    - fastify-multipart: For handling file uploads.
    - fastify-jwt: For JWT authentication.
    - fastify-swagger: For API documentation generation.
    - fastify-swagger-ui: For serving the Swagger UI.
    - fastify-cors: For handling CORS.
    
    It also sets up the environment variables and routes.

--- src/env/env.ts
+++ src/env/env.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 const envSchema = z.object({
   PORT: z.coerce.number(),
+  JWT_SECRET: z.string(),
+  AMBIENTE_SIGRA: z.enum(['dev','hom' ,'prod']),
 });
 
 export const env = envSchema.parse(process.env)
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { env } from "./env/env";
-import fastify, { FastifyReply } from "fastify";
+import { validatorCompiler, jsonSchemaTransform, serializerCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import fastifyMultipart from "@fastify/multipart";
+import fastify, { FastifyReply } from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifyJwt from "@fastify/jwt";
+import { env } from "./env/env";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -10,10 +14,59 @@ const HOST = '0.0.0.0'
 
 const size100MbFileSize = 1024 * 1024 * 100
 
-app.register(fastifyMultipart,{
+if (!env.JWT_SECRET) {
+  throw new Error('Variável de ambiente não encontrada!')
+}
+
+app.register(fastifyMultipart, {
   limits: {
     fileSize: size100MbFileSize
   }
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '60m'
+  }
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'WS-SIGRA API',
+      description: 'API de integração do sistema SIGRA',
+      version: '1.0.0',
+    },
+  },
+  exposeHeadRoutes: false,
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifyCors, {
+  origin: [
+    env.AMBIENTE_SIGRA === 'dev' ?
+      '*' :
+      'https://app.sigra.com.br', 'https://hom.sigra.com.br/',
+    'https://dev.sigra.com.br/'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+});
+
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+  uiConfig: {
+    operationsSorter: 'alpha',
+    tagsSorter: 'alpha'
+  },
 })
 
 app.listen({ port: PORT, host: HOST }, (err, addres) => {
@@ -24,6 +77,6 @@ app.listen({ port: PORT, host: HOST }, (err, addres) => {
   console.log(`Server listening at ${addres}`);
 });
 
-app.get('/health', async (___, res:FastifyReply) => {
-  res.status(200).send({ok:true});
+app.get('/health', async (___, res: FastifyReply) => {
+  res.status(200).send({ ok: true });
 });
